d87338dd7dea8659271f011a6061335c8b899f48
Markus Ketzer <markus.ketzer@softwarepark.cc>
2020-11-26 11:36:15 +0100

    Added solution for the second ML ops exercise

--- api.py
+++ api.py
@@ -1,17 +1,29 @@
 from flask import Flask, request
 from subprocess import Popen, PIPE
+from threading import Timer
 import json
+import os
 
 app = Flask(__name__)
+mlModel = "model2.py"
+timeout = 10
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    model = Popen(["python", "model1.py"], stdin=PIPE, stdout=PIPE, encoding='ascii')
-    model.stdout.readline()
-    lines = model.communicate(json.dumps(request.json))[0].splitlines()
-    result = next(x.replace('result: ', '', 1) for x in lines if x.startswith('result'))
-    return json.loads(result)
+    with Popen(["python", os.path.join("models", mlModel)], stdin=PIPE, stdout=PIPE, encoding='ascii') as model:
+        try:
+            model.stdout.readline()
+            timer = Timer(timeout, model.kill)
+            timer.start()
+            response = model.communicate(json.dumps(request.json), timeout=timeout)
+            timer.cancel()
+            lines = response[0].splitlines()
+            result = next(x.replace('result: ', '', 1) for x in lines if x.startswith('result'))
+            return json.loads(result)
+
+        except Exception:
+            return {'error': 'model timeout'}
 
 
 if __name__ == '__main__':
